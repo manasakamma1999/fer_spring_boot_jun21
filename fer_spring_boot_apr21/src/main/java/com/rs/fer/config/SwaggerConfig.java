f2910655282928640c1274df9a363ead8377af58
GowthamGanta <gowtham8866@gmail.com>
2021-08-21 19:51:04 +0530

    intial commit

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/config/SwaggerConfig.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/config/SwaggerConfig.java
@@ -20,4 +20,13 @@ public class SwaggerConfig {
                 .paths(PathSelectors.any())
                 .build();
     }
+    
+    @Bean
+    public Docket api1() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }

e7b6396766f5677cff340454934a9a0169deeec7
anitha <anithananna97@gmail.com>
2021-08-24 09:35:01 +0530

    To commit the login API

--- fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/UserController.java
+++ fer_spring_boot_apr21/src/main/java/com/rs/fer/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -46,6 +47,24 @@ public class UserController {
 		RegistrationResponse response = null;
 
 		Set<String> errorMessages = userValidation.validateRegistrationRequest(request);
+		
+		// return response with error messages
+		if (!CollectionUtils.isEmpty(errorMessages)) {
+			response = new RegistrationResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
+
+		} else {
+			response = userService.registration(request);
+		}
+		return response;
+	}
+
+	@PostMapping("/registrationma")
+	public RegistrationResponse registrationMA(@ModelAttribute @RequestBody RegistrationRequest request) {
+
+		RegistrationResponse response = null;
+
+		Set<String> errorMessages = userValidation.validateRegistrationRequest(request);
+		
 		// return response with error messages
 		if (!CollectionUtils.isEmpty(errorMessages)) {
 			response = new RegistrationResponse(HttpStatus.PRECONDITION_FAILED, "999", null, errorMessages);
